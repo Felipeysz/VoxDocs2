7069ef30628769168de3846d4ee9fb22cce25563
Felipeysz <Felipesantana87146520@gmail.com>
2025-04-23 00:05:29 -0300

    Segundo Commit Teste 2

--- obj/Release/net9.0/VoxDocs.AssemblyInfo.cs
+++ obj/Release/net9.0/VoxDocs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoxDocs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a93ce3b97bd46e3845758a9473375b8d747bb152")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoxDocs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoxDocs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
